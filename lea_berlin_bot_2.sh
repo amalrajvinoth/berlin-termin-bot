4457c921f1a0f214682936377fce59ffe0886910
Amal <amalrajvinoth@gmail.com>
2024-05-31 08:55:06 +0200

    activate venv on run

--- lea_berlin_bot_2.sh
+++ lea_berlin_bot_2.sh
@@ -2,4 +2,5 @@
 export PATH=$PATH:~/work/other/berlin-termin-bot
 export COLOREDLOGS_LEVEL_STYLES='spam=22;debug=28;verbose=34;notice=220;warning=202;success=118,bold;error=124;critical=background=red'
 export COLOREDLOGS_LOG_FORMAT='%(asctime)s,%(msecs)03d LEA %(levelname)s %(message)s'
+source ./venv/bin/activate
 LEA_FAMILY_REASON="Aufenthaltserlaubnis für Fachkräfte mit akademischer Ausbildung (§ 18b)" LEA_FAMILY_REASON_CATEGORY="Erwerbstätigkeit" python3 lea_berlin_bot.py
